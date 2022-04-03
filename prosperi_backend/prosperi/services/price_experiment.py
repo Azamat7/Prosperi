ebfd904d6db8a2488f71cf603c5ba161cdbff777
Azamat Smagulov <azamat@c02dx08nml87.zyxel.box>
2022-04-03 19:53:00 +0200

    refactor experiment allocation logic

--- prosperi_backend/prosperi/services/price_experiment.py
+++ prosperi_backend/prosperi/services/price_experiment.py
@@ -1,5 +1,18 @@
+from ..models import Bucket
+from ..utils import BucketTypes
+
+
 class PriceExperimentService(object):
     @staticmethod
     def get_price(pk):
-        # do the business logic here
-        return
+        user = Bucket.objects.filter(uuid = pk).first()
+        if user:
+            return user
+
+        bucket_a_count = Bucket.objects.filter(price = BucketTypes.A.value).count()
+        bucket_b_count = Bucket.objects.filter(price = BucketTypes.B.value).count()  
+        price = BucketTypes.A if bucket_a_count < bucket_b_count else BucketTypes.B
+        
+        new_user = Bucket(uuid=pk, price=price)
+        new_user.save()
+        return new_user
--- prosperi_backend/prosperi/views.py
+++ prosperi_backend/prosperi/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from .models import Bucket
 from .serializers import BucketSerializer
 
-import random
-from .utils import BucketTypes
+from .services.price_experiment import PriceExperimentService
 
 
 class BucketViewSet(ModelViewSet):
@@ -15,13 +13,6 @@ class BucketViewSet(ModelViewSet):
     permission_classes = [AllowAny]
 
     def retrieve(self, request, pk=None):
-        user = Bucket.objects.filter(uuid = pk).first()
-        if user:
-            serializer = self.serializer_class(user)
-            return Response(serializer.data)
-        
-        price = BucketTypes.A if random.randrange(100) < 50 else BucketTypes.B
-        new_user = Bucket(uuid=pk, price=price)
-        new_user.save()
-        serializer = self.serializer_class(new_user)
+        user = PriceExperimentService.get_price(pk)
+        serializer = self.serializer_class(user)
         return Response(serializer.data)
